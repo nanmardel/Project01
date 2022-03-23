db4e8c07d7a17c89b47e1f91c1f30ff6295a16b2
nanmardel <martin_n1@denison.edu>
2022-03-23 10:52:08 -0500

    mvp accomplished

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Begin by tracking initiation of game (listen for click) "Play" btn
-// inform Player to watch pattern before presenting pattern show msg "Watch"
+//inform Player to watch pattern before presenting pattern show msg "Watch"
 // Display color that needs to be clicked
 //create function to formulate random color patters using "blue", 
 //"red", "green", and "yellow"
@@ -14,10 +14,10 @@ const wrongEntry = 1;
 
 /*----------App's (State Variables)--------*/
 //DEFINE THE STATE
-// set values in our init fucntions
+// set values in init fucntions
 let randomSequence = []; //random color pattern from Colors
-let userEntry = []; //correct color clicked and what order it was clicked in
-let wrongSequence = [];
+let userEntry = []; //correct color clicked by user
+
 //set elements on the DOM that will need to be updated
 const colorBtns = document.querySelectorAll('.colors');
 const playBtn = document.querySelector('#play');
@@ -29,15 +29,18 @@ const correctMsg = document.querySelector('#Correct');
 document.querySelector('.colors').addEventListener('click', handleClick);
 playBtn.addEventListener('click', init);
 replayBtn.addEventListener('click', init);
-replayBtn.addEventListener('click',() => {
-replayBtn.style.display = 'none'});
+replayBtn.addEventListener('click', () =>{
+    replayBtn.innerText = "   "});
+    
+//replayBtn.addEventListener('click',() => {
+//replayBtn.style.display = 'none'});
 playBtn.addEventListener('click', () => {
     playBtn.style.display = 'none'});
 
 /*------------------FUNCTIONS-----------------*/
 //Initial controller function sets all the initial state vavlues (model)
 function init(e){
-    //console.log('init function is working') //DELETE
+    
 
     userEntry = [];
 
@@ -45,18 +48,15 @@ function init(e){
 
 const randomIndex = Math.floor(Math.random() * colors.length);
 let randomColor = colors[randomIndex];
-console.log(randomColor);  //DELETE
 
 randomSequence.push(randomColor);
-console.log(randomSequence);  //DELETE
 
-// Get sqaures to light up when user presses play to initiate randomSequence
+
+// Get sqaures to light up after user presses play btn to initiate randomSequence
 //loop through random sequence , setTimeOut, call function lightSquare
 let index = 0
 let interval = setInterval(() => {
     lightSquare(randomSequence[index]);
-    console.log(index);  //DELETE
-    console.log(randomSequence[index]);  //DELETE
     if(index + 1 === randomSequence.length){
         clearInterval(interval)
     }
@@ -66,39 +66,33 @@ let interval = setInterval(() => {
 
 
 function handleClick(e) {
-    console.log(e.target.id); //DELETE
-userEntry.push(e.target.id)
+    userEntry.push(e.target.id)
 
+//check to ensure user's entry matches computer's randomSequence, if wrong = gameover
 
 if(userEntry[userEntry.length -1] === randomSequence[userEntry.length -1]) {
     lightSquare(e.target.id);
-    
-
-    //console.log(userEntry);  // DELETE
-}else{
+    }else{
     gameOver()
+    }
     
-}
-    
+// keep track of when the user is done entering the randomSequence 
 
-// keep track of when the user is done immitatting the randomSequence 
 if(userEntry.length === randomSequence.length){
     init()
 }
 }
-
+//when a wrong square is clicked, do not allow user to click on squares, show "game over",return click
+//function and restart sequence
 function gameOver(){
     document.querySelector('.colors').removeEventListener('click', handleClick);
     document.getElementById("replay").innerText = "Game Over: Press To Replay";
     document.querySelector('.colors').addEventListener('click', handleClick);
     randomSequence = [];
-   
-    
-
-}
+    }
 
 
-// get the squares to light up for one second
+// Lightup suares for one second
 function lightSquare(color){
     document.getElementById(color).classList.add("light");
     setTimeout(() => document.getElementById(color).classList.remove("light"),1000);
